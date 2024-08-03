1ec1e79a797c12f4165de4e4a05a6181e5d6fa0d
Ibrahim18mib <ibrahimeblakwt@gmail.com>
2024-08-03 03:17:51 -1200

    added the access path of the frontend build in production environment

--- backend/app.js
+++ backend/app.js
@@ -16,6 +16,29 @@ app.use(express.json());
 app.use("/api/mib/", products);
 app.use("/api/mib/", order);
 
+//accessing the frontend with build path
+if (process.env.NODE_ENV == "production") {
+  app.use(
+    express.static(
+      path.join(__dirname, "..", "frontend", "dist", "frontend", "browser")
+    )
+  );
+  app.get("*", (req, res) => {
+    res.sendFile(
+      path.resolve(
+        __dirname,
+        "..",
+        "frontend",
+        "dist",
+        "frontend",
+        "browser",
+        "index.html"
+      )
+    );
+  });
+}
+//accessing END
+
 app.listen(process.env.PORT, () => {
   console.log(
     `Server Listening in PORT->${process.env.PORT} in ${process.env.NODE_ENV}...`
